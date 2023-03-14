647d715754b30ca6a98698a6743bbbf409d78479
Alejandro Fiesco <alfiesparra@hotmail.com>
2023-03-13 22:44:33 -0500

    Desastre arreglado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import BodyPrincipal from './Body-PaginaPrincipal/BodyPrincipal';
-
-import Header from './Header/Header';
-import { useState, useEffect } from 'react';
 import BodyNosotros from './Body-Nosotros/BodyNosotros';
 import BodyServicio from './Body-Servicio/BodyServicio';
-import BodyServicios from './Body-Servicios/BodyServicios';
 import BodyTemplate from './Bodies/Body-Template';
 
 
@@ -15,7 +11,7 @@ const App = () =>{
         <>
             <Routes>
                 <Route path='/' element={<BodyPrincipal/>}/>
-                <Route path='/:id' element={<BodyServicios />}/>
+                <Route path='/:id' element={<BodyServicio />}/>
                 <Route path='/Servicios/:seccion' element={<BodyTemplate/>}/>
                 <Route path='/Nosotros' element={<BodyNosotros />}/>
                 <Route path='/servicio/:id' element={<BodyServicio/> } />
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -18,17 +18,10 @@ function Header({color, nombre}) {
         </div> 
         <nav id='menu' className={mostrarNav ? 'mostrar' : 'noMostrar'}>
           <ul id='lista' style={{background: "linear-gradient(180deg,"+color+", transparent)"}} >
-<<<<<<< HEAD
-            <Link className='link' to='/2'>Hospedaje</Link>
-            <Link className='link' to='/1'>Comida</Link>
-            <Link className='link' to='/3'>Transporte</Link>
-            <Link className='link' to='/4'>Turismo</Link>
-=======
             <Link className='link' to='/Servicios/Hospedaje'>Hospedaje</Link>
             <Link className='link' to='/Servicios/Comida'>Comida</Link>
             <Link className='link' to='/Servicios/Transporte'>Transporte</Link>
             <Link className='link' to='/Servicios/Turismo'>Turismo</Link>
->>>>>>> f289bc4ce87960827b0a72a5da3bb464070bd907
             <Link className='link' to='/Nosotros'>Sobre Nosotros</Link>
           </ul>
         </nav>
